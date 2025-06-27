7f58d828db2190d955ef4df4a239dffad9fa2885
Vignesh <154775436+Vicky2505@users.noreply.github.com>
2025-06-27 15:23:18 +0530

    completed

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       home: MainPage(),
+      title: 'Banking App',
       debugShowCheckedModeBanner: false,
     );
   }
